24d286d6f2cbfc00a18980435c61810bc6e73bed
Anissa95 <f.anissa@hotmail.fr>
2021-10-12 12:16:23 +0200

    Importation du middleware password

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+//importaion du middleware/password 
+const password = require('../middleware/password')
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', password, userCtrl.signup);
+router.post('/login',userCtrl.login);
 
 module.exports = router;
